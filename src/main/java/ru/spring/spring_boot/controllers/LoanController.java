f4d9d322e3292435b9ca37ea9a0d646831848a4b
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-04-06 14:46:12 +0300

    refactoring

--- src/main/java/ru/spring/spring_boot/controllers/LoanController.java
+++ src/main/java/ru/spring/spring_boot/controllers/LoanController.java
@@ -26,9 +26,9 @@ public class LoanController {
     }
 
     @GetMapping()
-    public String index(@RequestParam(value = "id", required = false) Long id) {
+    public double index(@RequestParam(value = "id", required = false) Long id) {
         User user = userService.findOne(id);
         double loan = loanService.calculateLoan(user);
-        return "{\" loan \":\" %s \"}".formatted(loan);
+        return loan;
     }
 }
